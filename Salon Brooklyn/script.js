16d49366a186e8bf62b183f718e5130fa10a29ba
Fandy <85363581+fandoga@users.noreply.github.com>
2025-04-25 00:17:21 +0300

    Add files via upload

---
+++ Salon Brooklyn/script.js
@@ -0,0 +1,43 @@
+const menuLinks = document.querySelector('.menu_links');
+const signUpButton = document.querySelector('.button');
+
+
+let element;
+const offset = 50;
+let elementPosition
+let offsetPosition
+
+
+document.addEventListener('click', (event) => {
+    event.preventDefault()
+    Event = event.target
+    element = document.querySelector(Event.dataset.target);
+    elementPosition = element.getBoundingClientRect().top;
+    offsetPosition = elementPosition + window.pageYOffset - offset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+
+    console.log(Event.dataset.target);
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTop = document.getElementById("back_to_top");
+
+    // Показать/скрыть кнопку при прокрутке страницы
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 300) {
+            backToTop.style.bottom = "-30px";
+        } else {
+            backToTop.style.bottom = "-200px";
+        }
+    });
+
+    // Плавная прокрутка при клике на кнопку
+    backToTop.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
